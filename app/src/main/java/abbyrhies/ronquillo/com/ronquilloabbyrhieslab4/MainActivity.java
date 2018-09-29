9adc29a0367eb28714db2a1e93082b5e268c531f
Abbyrhies <abbyrhies.ronquillo.iics@ust.edu.ph>
2018-09-29 19:56:50 +0800

    Initial commit

---
+++ app/src/main/java/abbyrhies/ronquillo/com/ronquilloabbyrhieslab4/MainActivity.java
@@ -0,0 +1,66 @@
+package abbyrhies.ronquillo.com.ronquilloabbyrhieslab4;
+
+
+
+import android.nfc.Tag;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.SnackbarContentLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d( "4ITH", "onCreate: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d( "4ITH", "onStop: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d( "4ITH", "onStart: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d( "4ITH", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("4ITH", "onResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("4ITH", "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("4ITH", "onDestroy: ");
+    }
+
+    public void displayMsgNext(View n){
+        Snackbar.make(n, "hello again....", Snackbar.LENGTH_LONG) .show();
+    }
+
+    public void displayMsgBack(View b){
+        Toast.makeText(this, "Hello 4ITH", Toast.LENGTH_SHORT).show();
+    }
+}
